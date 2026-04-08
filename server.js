28a9ae35837e1cf488a216da7b600d939c97977e
xpixel2024 <167066824+xpixel2024@users.noreply.github.com>
2026-04-09 01:08:33 +0530

    index.html

--- server.js
+++ server.js
@@ -23,6 +23,8 @@ app.set("views", path.join(__dirname, "views"));
 // STATIC PAGES
 // ===================
 
+// ROUTES (CLEAN URLS)
+
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "public/index.html"));
 });
